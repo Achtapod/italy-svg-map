8ac34f75685bdc9425b109069782bb3297487562
buraktekin <tknbrk@gmail.com>
2020-07-01 18:34:29 +0200

    🔺 add styling for svg map

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Italy from "@svg-maps/italy";
 import { SVGMap } from "react-svg-map";
 import "react-svg-map/lib/index.css";
+import "./style.css";
 
 class App extends React.Component {
   constructor(props) {
