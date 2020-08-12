746b16f664491745c5c017ed60ac2d71a8ad5e7d
burak <tknbrk@gmail.com>
2020-08-12 17:42:17 +0200

    fix region filtering param for en, de

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Italy from "@svg-maps/italy";
 import { SVGMap } from "react-svg-map";
 import ReactTooltip from 'react-tooltip';
+import regionsJSON from "../public/italy_regions.json";
 import "react-svg-map/lib/index.css";
 import "./style.css";
 
@@ -11,8 +12,18 @@ class App extends React.Component {
     super(props);
   }
 
+  handleRegionName = (region) => () => {
+    const query = region.id
+    regionsJSON.map(item => {
+      const {de, en} = item;
+      if(query === de || query === en) {
+        region.names = `${de}|${en}`
+      }
+    })
+  }
+
   handleClick = value => () => {
-    window.top.location.href = `https://batuhancaksin.wixsite.com/website21/wine?Region=${value}`
+    window.top.location.href = `https://batuhancaksin.wixsite.com/website21/wine?Region=${value.names}`
   }
 
   render() {
@@ -20,12 +31,6 @@ class App extends React.Component {
       <div className='container centered'>
         <ReactTooltip />
         <svg className="svg-map" viewBox={Italy.viewBox}>
-          <defs>
-            <linearGradient id="MyGradient">
-              <stop offset="5%" stop-color="#F60" />
-              <stop offset="95%" stop-color="#FF6" />
-            </linearGradient>
-          </defs>
           {
             Italy.locations.map(region => {
               region.id = region.id.charAt(0).toUpperCase() + region.id.slice(1);
@@ -35,8 +40,9 @@ class App extends React.Component {
                 key={region.id} 
                 id={region.id} 
                 d={region.path}
-                onClick={this.handleClick(region.id)}
                 data-tip={region.id}
+                onClick={this.handleClick(region)}
+                onMouseEnter={this.handleRegionName(region)}
                 ></path>
               )
             })
